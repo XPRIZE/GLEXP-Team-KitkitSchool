3dff8d4fcb15fe8fd9799dcd39fa875eb9e3990d
Gunho Lee <gunho@enuma.com>
2017-09-30 17:57:45 -0700

    stop tracking obj files

--- mainapp/proj.android/obj/local/armeabi/objs-debug/cocos2dcpp_shared/__/__/Classes/Games/NumberTrace/Common/Basic/ScopeGuard.o.d
+++
@@ -1,8 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dcpp_shared/__/__/Classes/Games/NumberTrace/Common/Basic/ScopeGuard.o: \
- jni/../../Classes/Games/NumberTrace/Common/Basic/ScopeGuard.cpp \
- jni/../../Classes/Games/NumberTrace/Common/Basic/ScopeGuard.h \
- /Users/Sukkwon/Documents/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.9/include/functional
-
-jni/../../Classes/Games/NumberTrace/Common/Basic/ScopeGuard.h:
-
-/Users/Sukkwon/Documents/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.9/include/functional:
