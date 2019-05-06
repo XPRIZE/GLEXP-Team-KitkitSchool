0afa9c5c03630b405d2883f0c173bb2b46aff72e
Gunho Lee <gunho@enuma.com>
2019-05-05 21:02:27 -0700

    more .gitignore

--- mainapp/proj.android/obj/local/armeabi/objs-debug/cocos2dcpp_shared/__/__/Classes/Common/Basic/TodoSchoolNS.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dcpp_shared/__/__/Classes/Common/Basic/TodoSchoolNS.o: \
- jni/../../Classes/Common/Basic/TodoSchoolNS.cpp \
- jni/../../Classes/Common/Basic/TodoSchoolNS.h
-
-jni/../../Classes/Common/Basic/TodoSchoolNS.h:
