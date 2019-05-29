62a842c535e66cd47cca8bf6752cbf638a81abff
Meghna Pai <meghnap@maqsoftware.net>
2019-05-29 12:29:10 +0530

    Adjusted the font of English text

--- mainapp/Classes/Menu/DailyScene2.cpp
+++ mainapp/Classes/Menu/DailyScene2.cpp
@@ -868,14 +868,14 @@ void DailyScene2::setupFreechoiceTab()
             string labelHindi = labelName.substr(0, labelName.find(delim));
             string labelEnglish = labelName.substr(labelName.find(delim) + delim.length(),labelName.length()-1);
 
-            auto panelLabelHindi = TodoUtil::createLabelMultilineToFit(labelHindi, 90, Size(btnSize.width, 70), defaultFont, Color4B(255, 210, 74, 255*(avaliable ? 1 : 0.1)));
+            auto panelLabelHindi = TodoUtil::createLabelMultilineToFit(labelHindi, 90, Size(btnSize.width, 100), defaultFont, Color4B(255, 210, 74, 255*(avaliable ? 1 : 0.1)));
             panelLabelHindi->setAlignment(TextHAlignment::CENTER, TextVAlignment::CENTER);
             panelLabelHindi->setPosition(Vec2(btnSize.width/2, 80));
             gameBtn->addChild(panelLabelHindi);
 
-            auto panelLabelEnglish = TodoUtil::createLabelMultilineToFit(labelEnglish, 120, Size(btnSize.width, 120), fontName, Color4B(255, 210, 74, 255*(avaliable ? 1 : 0.1)));
+            auto panelLabelEnglish = TodoUtil::createLabelMultilineToFit(labelEnglish, 90, Size(btnSize.width, 100), fontName, Color4B(255, 210, 74, 255*(avaliable ? 1 : 0.1)));
             panelLabelEnglish->setAlignment(TextHAlignment::CENTER, TextVAlignment::CENTER);
-            panelLabelEnglish->setPosition(Vec2(btnSize.width/2, 20));
+            panelLabelEnglish->setPosition(Vec2(btnSize.width/2, 10));
             gameBtn->addChild(panelLabelEnglish);
             
             index++;
