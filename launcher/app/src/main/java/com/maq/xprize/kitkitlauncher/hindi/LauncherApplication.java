b1b0df4042f2e8bb3fec523bf7037b967ffb2d3e
SanchitTaliyan <sanchitt@maqsoftware.com>
2019-05-31 15:27:48 +0530

    Minor changes

--- launcher/app/src/main/java/com/maq/xprize/kitkitlauncher/hindi/LauncherApplication.java
+++ launcher/app/src/main/java/com/maq/xprize/kitkitlauncher/hindi/LauncherApplication.java
@@ -61,22 +61,33 @@ public class LauncherApplication extends Application {
         logger = new KitKitLogger(getPackageName(), getApplicationContext());
         dbHandler = new KitkitDBHandler(getApplicationContext());
 
-        if (dbHandler.numUser() == 0) {
+
+
+
+
+
+
+        //Start here
+
+      //  if (dbHandler.numUser() == 0) {
             //make users in DB
-            for (int i = 1; i <= 1; i++) {
-                User user = new User("user" + i, 0);
-//                user.setNumStars(i*50);
-//                user.setFinishTutorial(true);
-                dbHandler.addUser(user);
+        //    for (int i = 1; i <= 1; i++) {
+          //      User user = new User("user" + i, 0);
+//     no           user.setNumStars(i*50);
+//     no           user.setFinishTutorial(true);
+                //dbHandler.addUser(user);
 
-                if (i == 0) {
-                    dbHandler.setCurrentUser(user);
-                    currentUser = user;
-                    currentUsername = user.getUserName();
-                }
-            }
+                //if (i == 0) {
+                   // dbHandler.setCurrentUser(user);
+                    //currentUser = user;
+                    //currentUsername = user.getUserName();
+                //}
+            //}
 
-        }
+        //}
+
+        //ends here
+
         /*
         if (currentUser == null) {
             currentUser = dbHandler.getCurrentUser();
