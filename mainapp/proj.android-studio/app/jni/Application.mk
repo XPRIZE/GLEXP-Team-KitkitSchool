fd50d0faf247ca77d5d41458ae3820c1c48b6c9a
SanchitTaliyan <34193530+SanchitTaliyan@users.noreply.github.com>
2019-07-05 11:50:39 +0530

    Added 64-Bit architecture support. (#102)

--- mainapp/proj.android-studio/app/jni/Application.mk
+++ mainapp/proj.android-studio/app/jni/Application.mk
@@ -4,7 +4,7 @@ APP_SHORT_COMMANDS := true
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
 
-APP_ABI := armeabi
+APP_ABI := armeabi-v7a arm64-v8a
 
 ifeq ($(NDK_DEBUG),1)
   APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
